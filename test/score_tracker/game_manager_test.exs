10e0000b7ab67696beff4487334ca3b6b3a17de0
tylercannon <17668242+tylercannon@users.noreply.github.com>
2025-09-28 21:23:00 -0600

    more add_player tests

--- test/score_tracker/game_manager_test.exs
+++ test/score_tracker/game_manager_test.exs
@@ -3,7 +3,7 @@ defmodule ScoreTracker.GameManagerTest do
 
   alias NimbleOptions.ValidationError
   alias ScoreTracker.{GameManager, GameStorage}
-  alias ScoreTracker.GameType.Ripple
+  alias ScoreTracker.GameType.{Ripple, Rummy}
 
   setup context do
     opts = [
@@ -27,7 +27,7 @@ defmodule ScoreTracker.GameManagerTest do
         game_type: :custom,
         max_players: 4,
         max_rounds: 5,
-        players: ["Player 2", "Player 3"]
+        players: ["PlayerTwo", "PlayerThree"]
       ]
 
       game_id = GameManager.create_game(game_manager, game_opts)
@@ -46,8 +46,8 @@ defmodule ScoreTracker.GameManagerTest do
 
       player_names = Map.values(game.player_names)
       assert Enum.count(player_names) == 3
-      assert "Player 2" in player_names
-      assert "Player 3" in player_names
+      assert "PlayerTwo" in player_names
+      assert "PlayerThree" in player_names
       assert "Example" in player_names
 
       scores = Map.values(game.scores)
@@ -108,9 +108,9 @@ defmodule ScoreTracker.GameManagerTest do
         host_id: "game-host",
         host_name: "Example",
         game_mode: :party,
-        game_type: :ripple,
-        max_players: Ripple.max_players(),
-        max_rounds: Ripple.max_rounds()
+        game_type: :rummy,
+        max_players: Rummy.max_players(),
+        max_rounds: Rummy.max_rounds()
       ]
 
       Enum.each(invalid_game_opts, fn {key, value} ->
@@ -125,8 +125,9 @@ defmodule ScoreTracker.GameManagerTest do
   end
 
   describe "add_player/1" do
-    test "adds a player to a party game", %{game_manager: game_manager} do
-      game_opts = [
+    setup do
+      party_game_opts = [
+        allow_spectators: true,
         host_id: "game-host",
         host_name: "Example",
         game_mode: :party,
@@ -135,14 +136,100 @@ defmodule ScoreTracker.GameManagerTest do
         max_rounds: Ripple.max_rounds()
       ]
 
+      scorekeeper_game_opts = [
+        allow_spectators: true,
+        host_id: "game-host",
+        host_name: "Example",
+        game_mode: :scorekeeper,
+        game_type: :custom,
+        max_players: 4,
+        max_rounds: 5,
+        players: ["PlayerTwo", "PlayerThree"]
+      ]
+
+      %{party_game_opts: party_game_opts, scorekeeper_game_opts: scorekeeper_game_opts}
+    end
+
+    test "adds a player to a party game", %{
+      game_manager: game_manager,
+      party_game_opts: game_opts
+    } do
       game_id = GameManager.create_game(game_manager, game_opts)
 
-      player_opts = [game_id: game_id, player_id: "player1", player_name: "Player One"]
+      player_opts = [game_id: game_id, player_id: "player1", player_name: "PlayerOne"]
       :ok = GameManager.add_player(game_manager, player_opts)
 
       {:ok, game} = GameManager.get_game(game_manager, game_id)
-      assert game.player_names["player1"] == "Player One"
+      assert game.player_names["player1"] == "PlayerOne"
       assert game.scores["player1"] == %{}
+    end
+
+    test "adds a spectator to a scorekeeper game", %{
+      game_manager: game_manager,
+      scorekeeper_game_opts: game_opts
+    } do
+      game_id = GameManager.create_game(game_manager, game_opts)
+
+      player_id = "player1"
+      player_opts = [game_id: game_id, player_id: player_id, player_name: "PlayerOne"]
+      :ok = GameManager.add_player(game_manager, player_opts)
+
+      {:ok, game} = GameManager.get_game(game_manager, game_id)
+      refute player_id in game.player_names
+    end
+
+    test "adds a spectator to a party game", %{
+      game_manager: game_manager,
+      party_game_opts: game_opts
+    } do
+      # create and start a game to put it into the correct state
+      game_id = GameManager.create_game(game_manager, game_opts)
+      {:ok, :in_progress} = GameManager.start_game(game_manager, game_id: game_id)
+
+      player_id = "player1"
+      player_opts = [game_id: game_id, player_id: player_id, player_name: "PlayerOne"]
+      :ok = GameManager.add_player(game_manager, player_opts)
+
+      {:ok, game} = GameManager.get_game(game_manager, game_id)
+      refute player_id in game.player_names
+    end
+
+    test "rejects player that already exists in a party game", %{
+      game_manager: game_manager,
+      party_game_opts: game_opts
+    } do
+      game_id = GameManager.create_game(game_manager, game_opts)
+
+      player_opts = [game_id: game_id, player_id: "player1", player_name: "PlayerOne"]
+      # add the player once so it exists in the game state
+      :ok = GameManager.add_player(game_manager, player_opts)
+
+      assert {:error, :already_exists} == GameManager.add_player(game_manager, player_opts)
+    end
+
+    test "rejects adding spectator to a scorekeeper game", %{
+      game_manager: game_manager,
+      scorekeeper_game_opts: game_opts
+    } do
+      game_opts = Keyword.put(game_opts, :allow_spectators, false)
+      game_id = GameManager.create_game(game_manager, game_opts)
+
+      player_opts = [game_id: game_id, player_id: "player4", player_name: "PlayerFour"]
+      assert {:error, :not_joinable} == GameManager.add_player(game_manager, player_opts)
+    end
+
+    test "rejects adding spectator to a party game", %{
+      game_manager: game_manager,
+      party_game_opts: game_opts
+    } do
+      game_opts = Keyword.put(game_opts, :allow_spectators, false)
+
+      # create and start a game to put it into the correct state
+      game_id = GameManager.create_game(game_manager, game_opts)
+      {:ok, :in_progress} = GameManager.start_game(game_manager, game_id: game_id)
+
+      player_opts = [game_id: game_id, player_id: "player4", player_name: "PlayerFour"]
+      assert {:error, :not_joinable} == GameManager.add_player(game_manager, player_opts)
     end
   end
 
