012105bcc57c0ee77134d7663e86d1b7c4110851
tylercannon <17668242+tylercannon@users.noreply.github.com>
2025-11-25 23:33:35 -0700

    try updating prod endpoint config

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,14 +44,16 @@ if config_env() == :prod do
   config :score_tracker, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :score_tracker, ScoreTrackerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: port,
+      # For IPv6 support
+      transport_options: [socket_opts: [:inet6]]
     ]
 
   # ## SSL Support
