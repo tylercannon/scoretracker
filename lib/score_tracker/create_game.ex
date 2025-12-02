0886feb09854dc669b6c8aed665c634ae6f8c76c
tylercannon <17668242+tylercannon@users.noreply.github.com>
2025-12-01 19:31:34 -0700

    Prevent duplicate player names

--- lib/score_tracker/create_game.ex
+++ lib/score_tracker/create_game.ex
@@ -54,6 +54,7 @@ defmodule ScoreTracker.CreateGame do
     )
     |> validate_required([:host_name, :game_mode, :game_type])
     |> validate_player_name(:host_name)
+    |> validate_unique_player_names()
     |> validate_number(:max_players, greater_than_or_equal_to: 2, less_than_or_equal_to: 10)
     |> validate_number(:max_rounds, greater_than_or_equal_to: 1, less_than_or_equal_to: 20)
     |> put_built_in_game_info()
